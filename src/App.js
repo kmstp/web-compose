71a927d3b68f04a06f973bc0eb9d357691ba7064
Truong Hoang Dung <checkraiser11@gmail.com>
2018-06-24 18:08:16 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Elm from './elmWrapper';
+import { Signup } from './signup';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <Elm src={Signup} />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
