44c70b219351aaa7c5d1d6b02eca913e3027dcff
Truong Hoang Dung <checkraiser11@gmail.com>
2018-06-25 10:03:43 +0700

    Refactor router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Signup from './components/Signup';
+import Router from './Router';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Signup />
+        <Router />
       </div>
     );
   }
